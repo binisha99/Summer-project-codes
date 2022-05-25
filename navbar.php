4393763a8dc5b6c75e3e10e00e4231651cfd757b
Binaya <binayaboss2@gmail.com>
2022-05-25 18:13:03 +0545

    changes-in-navbar

--- navbar.php
+++ navbar.php
@@ -1,18 +1,19 @@
-
 <?php
-  echo'
-    <nav>
+echo '
+  <nav>
       <div class="logo">
-      <img src="img/logo.png">
+        <img src="img/logo.png">
       </div>
-    <ul>
+      <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="aboutus.php">About</li></a>
         <li><a href="contactus.php">Contact Us</li></a>
       </ul>
       <ul class="end">
-      <li><i class="fa-light fa-user-gear">Admin</i></li>
-</ul>
-    </nav>
+        <div class="container-gap">
+          <li><i class="fa-light fa-user-gear nav-admin">Admin</i></li>
+          <li><i class="fa-light fa-user-gear nav-user">User</i></li>
+        </div>
+    </ul>
+  </nav>
 ';
-?>
